fd1fec9aca8da1588e688a1381abe5cb4eb85181
Gagan Mishra <simonsimple305@gmail.com>
2025-12-29 14:07:41 +0530

    HTTP SErver completed

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,65 +1,117 @@
 package server
 
 import (
-	"fmt"
-	"io"
-	"net"
+    "bytes"
+    "fmt"
+    "io"
+    "main/internal/request"
+    "main/internal/response"
+    "net"
+    "sync"
 )
 
 type serverState string
+type HandlerError struct {
+    StatusCode 	response.StatusCode
+    Message 	string
+}
+
+type Handler func(w io.Writer, req *request.Request) *HandlerError 
 
 const (
-	StateInit    serverState = "init"
-	StateServing serverState = "serving"
-	StateClosed  serverState = "closed"
+    StateInit    serverState = "init"
+    StateServing serverState = "serving"
+    StateClosed  serverState = "closed"
 )
 
 type Server struct {
-	Port int
-
-	state serverState
+    Port int
+    handler Handler
+    listener net.Listener
+    state serverState
+    mu sync.Mutex
 }
 
-func newServer(p int) (s *Server) {
-	return &Server{
-		Port:  p,
-		state: StateInit,
-	}
+func newServer(p int, handler Handler) (s *Server) {
+    return &Server{
+        Port:  p,
+        handler: handler,  // Fix: actually assign the handler
+        state: StateInit,
+    }
 }
 
 func runConnection(s *Server, conn io.ReadWriteCloser) {
-	body := "Hello World!"
-	out := fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(body), body)
-	conn.Write([]byte(out))
-	conn.Close()
+    defer conn.Close()
+
+    headers := response.GetDefaultHeaders(0)
+    r, err := request.RequestFromReader(conn)
+    if err != nil {
+        response.WriteStatusLine(conn, response.StatusBadRequest)
+        response.WriteHeaders(conn, headers)
+        return
+    }
+
+    writer := bytes.NewBuffer([]byte{})
+    handlerError := s.handler(writer, r)
+
+    var body []byte = nil
+    var status response.StatusCode = response.StatusOk
+    if handlerError != nil {
+        status = handlerError.StatusCode
+        body = []byte(handlerError.Message)
+    } else {
+        body = writer.Bytes()
+    }
+
+    headers.Replace("Content-Length", fmt.Sprint(len(body)))
+
+    response.WriteStatusLine(conn, status)
+    response.WriteHeaders(conn, headers)
+    conn.Write(body)
 }
 
 func runServer(s *Server, l net.Listener) {
-	s.state = StateServing
-	for {
-		conn, err := l.Accept()
-		if s.state == StateClosed {
-			return 
-		}
-		if err != nil {
-			return
-		}
-		
-		go runConnection(s, conn)
-	}
+    s.mu.Lock()
+    s.state = StateServing
+    s.mu.Unlock()
+    
+    for {
+        conn, err := l.Accept()
+        
+        s.mu.Lock()
+        closed := s.state == StateClosed
+        s.mu.Unlock()
+        
+        if closed {
+            return 
+        }
+        if err != nil {
+            return
+        }
+        
+        go runConnection(s, conn)
+    }
 }
 
 func (s *Server) Close() {
-	s.state = StateClosed
+    s.mu.Lock()
+    s.state = StateClosed
+    listener := s.listener
+    s.mu.Unlock()
+    
+    if listener != nil {
+        listener.Close()
+    }
 }
 
-func Serve(port int) (*Server, error) {
-	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
-	if err != nil {
-		return nil, err
-	}
-	s := newServer(port)
-	go runServer(s, listener)
+func Serve(port int, handler Handler) (*Server, error) {
+    listener, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+    if err != nil {
+        return nil, err
+    }
+    s := newServer(port, handler)
+    s.listener = listener
+    go runServer(s, listener)
 
-	return s, nil
+    return s, nil
 }
